5393ec7b34ecfd9be242dd8b81b8b4738c1cfcbd
karinbellander <karinbellander@gmail.com>
2017-12-18 16:07:36 +0100

    Funktion

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -3,14 +3,63 @@ import { exVatToIncVat, incVatToExtVat } from "../calculations"
 
 class App extends React.Component {
 
-  render() {
-    return (
-      <div className="App">
-        <p>Example calculating ex vat for 1000kr inc vat @ 25%: {incVatToExtVat(25, 1000)}</p>
-        <p>Example calculating inc vat for 600kr ex vat @ 6%: {exVatToIncVat(6, 600)}</p>
-      </div>
-    )
+  constructor(props) {
+    super(props)
+    this.state = {
+      incVat: 0,
+      exVat: 0,
+      vatRate: 25
+    }
   }
+
+handleIncVat = event => {
+  this.setState({
+    incVat: parseInt(event.target.value, 10),
+    exVat: incVatToExtVat(this.state.vatRate, parseInt(event.target.value, 10))
+  })
+}
+
+handleExVat = event => {
+  this.setState({
+    exVat: parseInt(event.target.value, 10),
+    IncVat: exVatToIncVat(this.state.vatRate, parseInt(event.target.value, 10))
+  })
+}
+
+handleVatRate = event => {
+  this.setState({
+    vatRate: parseInt(event.target.value, 10),
+    exVat: incVatToExtVat(this.state.vatRate, parseInt(event.target.value, 10)),
+    IncVat: exVatToIncVat(this.state.vatRate, parseInt(event.target.value, 10))
+
+  })
+}
+
+render() {
+  return (
+    <div className="App">
+      <div className="container-radio-vat">
+        <form>
+          <input type="radio" name="vatRate" onChange={this.handleVatRate} value="25" id="vat-25" />
+          <label htmlFor="vat-25">25%</label>
+          <input type="radio" name="vatRate" onChange={this.handleVatRate} value="12" id="vat-12" />
+          <label htmlFor="vat-12">12%</label>
+          <input type="radio" name="vatRate" onChange={this.handleVatRate} value="6" id="vat-6" />
+          <label htmlFor="vat-6">6%</label>
+        </form>
+      </div>
+
+      <div className="">
+        <form>
+          <label>Inklusive moms</label>
+          <input type="number" name="incVat" value={this.state.incVat} onChange={this.handleIncVat} />
+          <label>Exklusive moms</label>
+          <input type="number" name="excVat" value={this.state.exVat} onChange={this.handleExVat} />
+        </form>
+      </div>
+    </div>
+  )
+}
 
 }
 
